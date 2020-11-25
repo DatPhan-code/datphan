74c8f0957e5633a86bb96a9012752352e36a11d1
Dat Phan <datphan@Dats-MacBook-Pro.local>
2020-11-25 22:25:47 +0700

    bai12 update

--- baitap11.py
+++ baitap11.py
@@ -1,3 +1,4 @@
+import enum
 import zlib
 
 s ="hello helloo helloo"
@@ -13,4 +14,36 @@ li = [1,23.4]
 shuffle(li)
 print(li)
 
+#list comprehension
+
+li =[12,24,35,70,88,120,155]
+li2 =[]
+for x in li:
+    if x % 5!=0 and x %7!=0:
+        li2.append(str(x))
+print(",".join(li2))
+
+li3 =[x for x in li if x %5!=0 and x%7!=0]
+print(li3)
+
+li4 = [2,3,5,6,8,14]
+li5 =[x for x in li4 if x%2!=0]
+print (li5)
+
+
+li = [12,24,35,70,88,120,155]
+ # Code by Quantrimang.com
+a= [x for i,x in enumerate(li)if i%2!=0]
+print (a)
+
+
+
+
+     
+    
+        
+
+        
+    
+    
 
---
+++ baitap12.py
@@ -0,0 +1,7 @@
+li = [12,24,35,70,88,120,155]
+a =[]
+for x in enumerate(li):
+    for i in enumerate(li):
+        if i%2!=0:
+            a.append[i]
+print(tuple(a))
